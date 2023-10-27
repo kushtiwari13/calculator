55bbaa969281994dbe9f631a1783a833c66a4053
Kush Tiwari <tiwarikush9328@gmail.com>
2023-10-27 16:31:41 +0530

    second commit in master branch

--- first.java
+++ first.java
@@ -2,5 +2,7 @@ class first{
     public static void main(String[] args) {
         System.out.println("hii kush");
         System.out.println("hii kush");
+
+        System.out.println("second commit in master");
     }
 }
